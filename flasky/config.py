4475642801d967a069b887d586a400501d73979a
tage <763409282@qq.com>
2016-04-20 20:20:26 +0800

    delete private message

--- flasky/config.py
+++ flasky/config.py
@@ -7,15 +7,15 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'lpk0628@163.com'
-    FLASKY_ADMIN = '763409282@qq.com'
+    FLASKY_MAIL_SENDER = ''
+    FLASKY_ADMIN = ''
     BOOTSTRAP_SERVE_LOCAL = True
 
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'lpk0628@163.com'
-    MAIL_PASSWORD = '0628lupengkai'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
 
     @staticmethod
     def init_app(app):
